d320b3e1a318be300c4c012fd63f64d25b9eb385
Citlali767 <citlah06@gmail.com>
2026-09-08 11:47:16 -0600

    funcionFactorial en pantalla

--- factorialFor.js
+++ factorialFor.js
@@ -13,3 +13,13 @@ console.log(factorial(10)); // salida: 120
 
 .
 console.log(factorial(5)); // salida: 120
+function mostrarSerieFactorial(n) {
+    let resultado = 1;
+    for (let i = 1; i <= n; i++) {
+        resultado = resultado * i;
+        console.log(`Factorial de ${i} = ${resultado}`);
+    }
+}
+
+// ejemplo de uso
+mostrarSerieFactorial(10);
